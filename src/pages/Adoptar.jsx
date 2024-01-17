39d3cfa05fd153115c45621de02b57180dd8f61c
Lulubonucci <bonucci64@gmail.com>
2024-01-17 15:51:16 +0100

    modif de dbjson, ruta get axios

--- src/pages/Adoptar.jsx
+++ src/pages/Adoptar.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import AnimalCard from "../components/card/AnimalCard";
+import axios from "axios";
 
 
 const Adoptar = () => {
+  const [animales, setAnimales] = useState([]);
+  useEffect(() => {
+    const data = async () => {
+      const response = await axios.get("http://localhost:3000/results"
+      );
+      const info = await response.data;
+      setAnimales(info);
+      console.log(info)
+    }
+
+    data()
+  }, [])
+
   return (
-    <div> soy Adoptar</div>
+    <>
+      {animales.map((animal) => {
+        return <AnimalCard key={animal.id} animal={animal} />
+      })}
+    </>
   )
 }
 
