b6514e5a9a6e5fbf95d1d359fca5ee969e941844
2007riot <2007riot@gmail.com>
2024-01-22 14:35:35 +0100

    filter logic implemented

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,14 +1,85 @@
+import { useState } from "react"
 import "./Filter.css"
-import FilterField from "./FilterField/FilterField"
-const Filter = () => {
+
+const Filter = ({ onClick }) => {
+    const [isAnimalListOpen, setAnimalListOpen] = useState(false)
+    const [isTamanoListOpen, setTamanoListOpen] = useState(false)
+    const [isProvinciaListOpen, setProvinciaListOpen] = useState(false)
+    const [isEdadListOpen, setEdadListOpen] = useState(false)
+
+    const toggleAnimal = () => {
+        setAnimalListOpen(!isAnimalListOpen)
+        setTamanoListOpen(false)
+        setProvinciaListOpen(false)
+        setEdadListOpen(false)
+    }
+
+    const toggleTamano = () => {
+        setTamanoListOpen(!isTamanoListOpen)
+        setAnimalListOpen(false)
+        setProvinciaListOpen(false)
+        setEdadListOpen(false)
+    }
+    const toggleProvincia = () => {
+        setProvinciaListOpen(!isProvinciaListOpen)
+        setAnimalListOpen(false)
+        setTamanoListOpen(false)
+        setEdadListOpen(false)
+    };
+
+    const toggleEdad = () => {
+        setEdadListOpen(!isEdadListOpen)
+        setAnimalListOpen(false)
+        setTamanoListOpen(false)
+        setProvinciaListOpen(false)
+    };
+
     return (
         <div className="filter">
-            <FilterField value="Animal" />
-            <FilterField value="Tomaño" />
-            <FilterField value="Provincia" />
-            <FilterField value="Edad" />
+            {/* Animal filter */}
+            <div className="filterField">
+                <span onClick={toggleAnimal}>Animal</span>
+                {isAnimalListOpen ? (
+                    <ul>
+                        <li onClick={() => onClick("tipo", "perro")}>Perro</li>
+                        <li onClick={() => onClick("tipo", "gato")}>Gato</li>
+                    </ul>
+                ) : null}
+            </div>
+
+            {/* Tamaño filter */}
+            <div className="filterField">
+                <span onClick={toggleTamano}>Tomaño</span>
+                {isTamanoListOpen ? (
+                    <ul>
+                        <li onClick={() => onClick("tamano", "Pequeño")}>Pequeño</li>
+                        <li onClick={() => onClick("tamano", "Mediano")}>Mediano</li>
+                        <li onClick={() => onClick("tamano", "Grande")}>Grande</li>
+                    </ul>
+                ) : null}
+            </div>
+
+            {/* Provincia filter */}
+            <div className="filterField">
+                <span onClick={toggleProvincia}>Provincia</span>
+                {isProvinciaListOpen ? (
+                    <ul>
+                        <li onClick={() => onClick("ubicacion", "Barcelona")}>Barcelona</li>
+                    </ul>
+                ) : null}
+            </div>
+
+            <div className="filterField">
+                <span onClick={toggleEdad}>Edad</span>
+                {isEdadListOpen ? (
+                    <ul>
+                        <li onClick={() => onClick("edad", "Cachorrito")}>Cachorrito</li>
+                        <li onClick={() => onClick("edad", "Adulto")}>Adulto</li>
+                    </ul>
+                ) : null}
+            </div>
         </div>
     )
 }
 
-export default Filter
+export default Filter;
