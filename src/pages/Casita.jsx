86f4d29a2da6e40b14304216bb1fbb4ca3a7d344
indiakka <78439130+indiakka@user.noreply.git>
2024-01-23 11:13:00 +0100

    creación rama feature/casita e inicio de archivo casita.jsx

--- src/pages/Casita.jsx
+++ src/pages/Casita.jsx
@@ -1,8 +1,14 @@
 import Formulario from "../components/formulario/Formulario";
+import {animal} from '../components/card/AnimalCard'
 
 const Casita = () => {
   return (
     <>
+     {/*  <div className="contenedor">
+        <img src={animal.image} />
+        <h2>{animal.nombre}</h2>
+        <p>{animal.edad}{animal.ubicacion}</p>
+      </div> */}
       <Formulario />
     </>
   )
