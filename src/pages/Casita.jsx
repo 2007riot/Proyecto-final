1f0f01e6776f6169279e64a2db46e5d2ad703bb4
indiakka <78439130+indiakka@user.noreply.git>
2024-01-24 10:50:29 +0100

    creación de desplegable para añadir a la casita según los animales que vayamos agregando

--- src/pages/Casita.jsx
+++ src/pages/Casita.jsx
@@ -1,15 +1,44 @@
-import Formulario from "../components/formulario/Formulario";
-import {animal} from '../components/card/AnimalCard'
+import './casita.css';
 
-const Casita = () => {
+
+
+const Casita = () =>
+{
   return (
     <>
-     {/*  <div className="contenedor">
-        <img src={animal.image} />
-        <h2>{animal.nombre}</h2>
-        <p>{animal.edad}{animal.ubicacion}</p>
-      </div> */}
-      <Formulario />
+      <div className='contenedor-casita-card'>
+        <div className='casita-card'>
+          <div className='contenedor-x-card'>
+          <img src='src/assets/images/x_card.svg' className='x-card'  alt='descartar-animal'/>
+          </div>
+          <img className='casita-imagen' alt='imagen_animal' src='https://images.pexels.com/photos/104115/pexels-photo-104115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+          <div className='casita-texto'>
+            <h4>Ginger</h4>
+            <h5>2 años, Barcelona</h5>
+          </div>
+        </div>
+        <div className='casita-card'>
+          <div className='contenedor-x-card'>
+            <img src='src/assets/images/x_card.svg' className='x-card' alt='descartar-animal' />
+          </div>
+          <img className='casita-imagen' alt='imagen_animal' src='https://images.pexels.com/photos/104115/pexels-photo-104115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+          <div className='casita-texto'>
+            <h4>Ginger</h4>
+            <h5>2 años, Barcelona</h5>
+          </div>
+        </div>
+        <div className='casita-card'>
+          <div className='contenedor-x-card'>
+            <img src='src/assets/images/x_card.svg' className='x-card' alt='descartar-animal' />
+          </div>
+          <img className='casita-imagen' alt='imagen_animal' src='https://images.pexels.com/photos/104115/pexels-photo-104115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+          <div className='casita-texto'>
+            <h4>Ginger</h4>
+            <h5>2 años, Barcelona</h5>
+          </div>
+        </div>
+        <button type='submit' className='casita-boton'>Reservar cita</button>
+      </div>
     </>
   )
 }
