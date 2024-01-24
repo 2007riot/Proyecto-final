d3008128d8f1dcd5f040cff9f7e2983d261b6969
dmnchmr <marformaciio@gmail.com>
2024-01-24 10:49:11 +0100

    creación estilos de animal info

---
+++ src/pages/animalInfo/AnimalInfo.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useState } from "react";
+import "./animalInfo.css";
+
+const AnimalInfo = () => {
+  const [animal, setAnimal] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const animalInfo = async () => {
+      const response = await axios.get(`http://localhost:3000/results/${id}`);
+      setAnimal(response.data);
+    };
+    animalInfo();
+  }, [id]);
+  return (
+    <div className="animalInfocontainer">
+      <div className="animalInfoImg--container">
+        <img
+          src={animal.imagen}
+          alt={animal.nombre}
+          className="animalInfo--img"
+        />
+      </div>
+      <div className="animalInfoTxt--container">
+        <h2>Información sobre {animal.nombre}</h2>
+        <p>Tipo: {animal.tipo}</p>
+        <p>Raza: {animal.raza}</p>
+        <p>Tamaño: {animal.tamaño}</p>
+        <p>Cuidados Especiales: {animal.cuidadosEspeciales}</p>
+        <p>Ubicación: {animal.ubicacion}</p>
+        <p>Años: {animal.años}</p>
+        <p>Gastos de Gestión: {animal.gastosDeGestion}</p>
+        <div>
+        <button className="button-adopta">
+          <img src="src/assets/iconoConoceme.png" alt="iconoConoceme" />
+          Conóceme</button>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default AnimalInfo;
