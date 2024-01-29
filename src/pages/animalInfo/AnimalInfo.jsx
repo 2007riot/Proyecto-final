46e3b31109cdf9cfbf70a8a7686d076e16a8f98f
Lulubonucci <bonucci64@gmail.com>
2024-01-29 14:02:36 +0100

    casita-localStorage-

--- src/pages/animalInfo/AnimalInfo.jsx
+++ src/pages/animalInfo/AnimalInfo.jsx
@@ -4,33 +4,36 @@ import { useParams } from "react-router";
 import { useState } from "react";
 import "./animalInfo.css";
 
-const AnimalInfo = () =>
-{
-  const [ animal, setAnimal ] = useState( [] );
+const AnimalInfo = () => {
+  const [animal, setAnimal] = useState([]);
   const { id } = useParams();
-  useEffect( () =>
-  {
-    const animalInfo = async () =>
-    {
-      const response = await axios.get( `http://localhost:3000/results/${id}` );
-      setAnimal( response.data );
+  const [animalesCasita, setAnimalesCasita] = useState([]);
+
+  useEffect(() => {
+    const animalInfo = async () => {
+      const response = await axios.get(`http://localhost:3000/results/${id}`);
+      setAnimal(response.data);
     };
     animalInfo();
-  }, [ id ] );
+  }, [id]);
 
-  const [ animalesCasita, setAnimalesCasita ] = useState( [] )
-  
-  const anadirAnimal = () =>
-  {
-    const listadoAnimales = [ ...animalesCasita, animal ];
-    setAnimalesCasita( listadoAnimales );
-  /*   // Asegúrate de que el animal no esté ya en la lista antes de agregarlo
-    if ( !animalesCasita.some( ( a ) => a.id === animal.id ) )
-    {
-      setAnimalesCasita( [ ...animalesCasita, animal ] );
-    } */
+  const anadirAnimal = () => {
+    const listadoAnimales = [...animalesCasita, { ...animal, id: animal.id }];
+    setAnimalesCasita(listadoAnimales);
   };
-  console.log( animalesCasita )
+
+  useEffect(() => {
+    const animalesAlmacenados = localStorage.getItem('animalesCasita');
+    if (animalesAlmacenados) {
+      setAnimalesCasita(JSON.parse(animalesAlmacenados));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('animalesCasita', JSON.stringify(animalesCasita));
+  }, [animalesCasita]);
+
+  console.log(animalesCasita)
 
   return (
     <div className="animalInfocontainer">
@@ -51,14 +54,15 @@ const AnimalInfo = () =>
         <p>Años: {animal.años}</p>
         <p>Gastos de Gestión: {animal.gastosDeGestion}</p>
         <div className="container--button">
-          <button onClick={anadirAnimal} className="button-adopta btn--conoceme">
-            <img src="../src/assets/iconoConoceme.png" alt="iconoConoceme" className="iconoConoceme" />
-            Conóceme</button>
+          <div className="container--button">
+            <button onClick={anadirAnimal} className="button-adopta btn--conoceme">
+              <img src="../src/assets/iconoConoceme.png" alt="iconoConoceme" className="iconoConoceme" />
+              Conóceme</button>
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
 
-export default AnimalInfo;
+export default AnimalInfo
