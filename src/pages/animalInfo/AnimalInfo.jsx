a9356367f595b996c65662558451626b4407a58c
indiakka <78439130+indiakka@user.noreply.git>
2024-01-29 16:08:43 +0100

    solucionar el AnimalInfo quitar los filtros de más

--- src/pages/animalInfo/AnimalInfo.jsx
+++ src/pages/animalInfo/AnimalInfo.jsx
@@ -1,40 +1,44 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useState } from "react";
 import "./animalInfo.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
 
-const AnimalInfo = () => {
-  const [animal, setAnimal] = useState([]);
+const AnimalInfo = () =>
+{
+  const [ animal, setAnimal ] = useState( {} );
   const { id } = useParams();
-  const [animalesCasita, setAnimalesCasita] = useState([]);
-
-  useEffect(() => {
-    const animalInfo = async () => {
-      const response = await axios.get(`http://localhost:3000/results/${id}`);
-      setAnimal(response.data);
+  const navigate = useNavigate()
+  useEffect( () =>
+  {
+    const animalInfo = async () =>
+    {
+      const response = await axios.get( `http://localhost:3000/results/${id}` );
+      setAnimal( response.data );
     };
     animalInfo();
-  }, [id]);
+  }, [ id ] );
 
-  const anadirAnimal = () => {
-    const listadoAnimales = [...animalesCasita, { ...animal, id: animal.id }];
-    setAnimalesCasita(listadoAnimales);
-    alert("Animal añadido a tu casita")
-  };
-
-  useEffect(() => {
-    const animalesAlmacenados = localStorage.getItem('animalesCasita');
-    if (animalesAlmacenados) {
-      setAnimalesCasita(JSON.parse(animalesAlmacenados));
+  const eliminarAnimal = async ( id ) =>
+  {
+    const conf = window.confirm( '¿Quieres realmente borrar este animal?' )
+    if ( conf )
+    {
+      await axios.delete( `http://localhost:3000/results/${id}` )
+      alert( 'Este animal ha sido borrado correctamente' )
+      navigate( '/adoptar' )
     }
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem('animalesCasita', JSON.stringify(animalesCasita));
-  }, [animalesCasita]);
+  }
+
+  const [ animalesCasita, setAnimalesCasita ] = useState( [] )
+
+  const anadirAnimal = () =>
+  {
+    const listadoAnimales = [ ...animalesCasita, animal ];
+    setAnimalesCasita( listadoAnimales );
+  };
 
 
   return (
@@ -56,14 +60,21 @@ const AnimalInfo = () => {
         <p>Años: {animal.años}</p>
         <p>Gastos de Gestión: {animal.gastosDeGestion}</p>
         <div className="container--button">
-          <div className="container--button">
-            <button onClick={anadirAnimal} className="button-adopta btn--conoceme">
-              <FontAwesomeIcon icon={faPaw} />Conóceme</button>
-          </div>
+          <button onClick={anadirAnimal} className="button-adopta btn--conoceme">
+            <img src="../src/assets/images/iconoConoceme.png" alt="iconoConoceme" className="iconoConoceme" />
+            Conóceme</button>
         </div>
       </div>
+      <div className="contenedor--botones--editar">
+        <NavLink to={`/editarInfo/${animal.id}`}>
+          <button className="botones--editar">
+            <img src="../src/assets/images/Edit.png" alt="editar" /></button>
+        </NavLink>
+        <button onClick={e => eliminarAnimal( animal.id )} className="botones--editar">
+          <img src="../src/assets/images/Delete.png" alt="borrar" /></button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default AnimalInfo
+export default AnimalInfo;
